75e6f8ad5add180311db1b96365f3ba0f3d65148
Krish <krishm848@gmail.com>
2024-07-12 14:44:29 +0530

    Added comments and future tasks for self

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+# Module imports
 import logging
 import docker
 from datetime import datetime
 
-def initialise_log():
+# Initialise log file. This will overwrite the last log and make a new one. If you want that the previous log dont get deleted
+# then replace f = open('output.log', 'w') with  f = open('output.log', 'a'). This will generate new logs inside same file without overwrites.
+def initLog():
     f = open('output.log', 'w')
     f.writelines('\nLOG '+str(datetime.now())+'\n')
+    f.close()
 
-def read_log():
+# Outputs contents of output.log in the terminal window. Each line of log is generated in this format: INFO:root:b'<output of command>\n'
+# so to make it readable in terminal, decided to remove the extra parts 
+def readLog():
     f = open('output.log', 'r')
     x = f.readlines()
     filter = ["INFO:root:b'", "\\n'"]
@@ -17,17 +23,25 @@ def read_log():
         print(filtered)            
     f.close()
 
-if __name__ == "__main__":
-    initialise_log()
-    logging.basicConfig(filename='output.log', level=logging.INFO)
+def main():
     client = docker.from_env()
+    initLog()
+    logging.basicConfig(filename='output.log', level=logging.INFO)    
     logging.info(client.containers.run("kalilinux/kali-rolling", "echo hello world"))
-    read_log()
+    readLog()
+
+if __name__ == "__main__":
+    main()
     
     
-    
-    # PLAN FROM MY SIDE
-    # 1. Automate testing [DONE]
-    # 2. Store report in a log file [DONE]
-    # 3. Clear log each run [DONE]
-    # 4. Output contents of log excluding [INFO] and \n [DONE]
+# PLAN FROM MY SIDE
+# 1. Automate testing [DONE]
+# 2. Store report in a log file [DONE]
+# 3. Clear log each run [DONE]
+# 4. Output contents of log excluding [INFO] and \n [DONE]
+# 5. Let the clean log be sent to JS for display
+# 6. Help with OpenAI integration to explain the vulnerabilities
+# 7. Extract links from Nikto logs
+# 8. Error and Exception handling 
+# 9. Set the port of website hosted to something which do not interfere with other services
+# 10. Ask user to give a part incase the default port is used already
