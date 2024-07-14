f491395a39668b7ef199c4d744ee2198d7937e65
redguyig <144947807+redguyig@users.noreply.github.com>
2024-07-14 19:07:17 +0530

    Updated hi.bat

--- hi.bat
+++ hi.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 docker build -t 'test' --file 'C:\Users\Vaibhav Kathait\Desktop\mern1\webscan\Dockerfile'
+:: this if for the local file but idk y i was facing error while using it as a script so yeah ig figure out something"
+::since the above command won't work the actual command is 
+::docker build -t 'test' .
 
 docker run --name "nikto" test
 docker start nikto
