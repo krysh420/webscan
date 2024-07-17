0bf5e5cb34d5d1a27afa958fe184a1057244d5c4
Krish <krishm848@gmail.com>
2024-07-17 21:20:26 +0530

    Didnt commit properly

--- app.py
+++ app.py
@@ -13,21 +13,19 @@ from datetime import datetime
 from re import findall 
 
 # Declare log name
-logName = 'LOG-'+str(datetime.now())+'.log' 
+LOGNAME = 'LOG-'+str(datetime.now())+'.log' 
 
 # Initialise log file.
-def initLog():
-    f = open(f'logs/{logName}', 'w')
+def init_log():
+    f = open(f'logs/{LOGNAME}', 'w')
     f.writelines('\nLOG '+str(datetime.now()))
     f.close()
 
 
 # Outputs contents of output.log in the terminal window. Each line of log is generated in this format: INFO:root:b'<output of command>\n'
 # so to make it readable in terminal, decided to remove the extra parts 
-def readLog():
-    global logName
-
-    f = open(f'logs/{logName}', 'r')
+def read_log():
+    f = open(f'logs/{LOGNAME}', 'r')
     
     x = f.readlines()
     filter = ["INFO:root:"]
@@ -39,12 +37,10 @@ def readLog():
     f.close()
 
 
-def extractLinks():
-    global logName
-
+def extract_links():
     f = open('links.txt', 'w')
 
-    with open(f'logs/{logName}') as file:
+    with open(f'logs/{LOGNAME}') as file:
         for line in file:
             urls = findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
             f.write(str(urls)+'\n')
@@ -52,21 +48,19 @@ def extractLinks():
     f.close()
 
 def main():
-    global logName
-
 # You change the client path by uncommenting the line below and putting your address to 
     # client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     client = docker.from_env() # be sure to comment this line when you do that
     container = client.containers.run("kalilinux/kali-rolling", "apt update", detach=True)
     
-    initLog()
-    logging.basicConfig(filename=f'logs/{logName}',filemode='a', level=logging.INFO)    
+    init_log()
+    logging.basicConfig(filename=f'logs/{LOGNAME}',filemode='a', level=logging.INFO)    
     
     for line in container.logs(stream=True):
         logging.info(line.strip().decode('utf-8')) # To have clean logs
     
-    readLog()
-    extractLinks()
+    read_log()
+    extract_links()
 
 if __name__ == "__main__":
     main()
@@ -75,7 +69,7 @@ if __name__ == "__main__":
 # PLAN FROM MY SIDE
 # 1. Automate testing [DONE]
 # 2. Store report in a log file [DONE]
-# 3. Clear log each run [DONE]
+# 3. Clear log each run [DONE] {NOT NEEDED NOW}
 # 4. Output contents of log excluding [INFO] and \n [DONE]
 # 5. Let the clean log be sent to JS for display [DONE]
 # 6. Help with OpenAI integration to explain the vulnerabilities 
--- setup.py
+++ setup.py
@@ -7,32 +7,34 @@ from platform import system
 # To timestamp last setup
 from datetime import datetime
 
-def setupReq():
-    global imgName 
-    imgName = "nikto-img"
-    global platform
+def setup_req():
+    global IMG_NAME 
+    IMG_NAME = "nikto-img"
+    global PLATFORM
     print("Checking platform....")
-    platform = system()
-    print("Detected OS: ", platform)
+    PLATFORM = system()
+    print("Detected OS: ", PLATFORM+"\n")
 
     print("Make sure that you have downloaded and installed Docker or Podman (NOTE: Docker/Podman Desktop is not required). Setup cannot proceed without it.")
-    global engine
-    engine = input("Enter what did you install (Docker or Podman): ")
+    global ENGINE
+    ENGINE = input("Enter what did you install (Docker or Podman): ")
 
     while(True):
-        if engine.lower() == "docker":
+        if ENGINE.lower() == "docker":
             os.system("docker run hello-world")
-        elif engine.lower() == "podman":
+        elif ENGINE.lower() == "podman":
             os.system("podman run hello-world")
         else:
             print("The given engine either does not exist or is not supported by this project at the moment.")
+            print("Terminating setup...")
+            quit()
         
         test = input("Did the 'Hello from Docker' executed properly? [It will show hello from docker for both podman and docker] (Yes/No): ")
         if test.lower() == "yes":
-            print(f"{engine.title()} has been succesfully installed.")
+            print(f"{ENGINE.title()} has been succesfully installed.\n")
             break
         elif test.lower() == "no":
-            print(f"""Make sure {engine.title()} is installed, its services are online and its added to PATH (More important for Windows users).
+            print(f"""Make sure {ENGINE.title()} is installed, its services are online and its added to PATH (More important for Windows users).
 Make sure you are connected to the internet.
 Make sure you have sufficient privileges.
 Retry the test.  """)
@@ -44,17 +46,17 @@ Retry the test.  """)
     f = open("requirements.txt", "r")
     lines = f.readlines()
     for i in lines:
-        if engine.lower() == "docker":
+        if ENGINE.lower() == "docker":
             if "podman" in i:
                 continue                
-        elif engine.lower() == "podman":
+        elif ENGINE.lower() == "podman":
             if "docker" in i:
                 continue
         os.system(f"python -m pip install {i}")
     while True:
         installed = input("Did all packages installed successfully? (Yes/No): ")
         if installed.lower() == "yes":
-            print("Setup will now continue...... ")
+            print("Setup will now continue...... \n")
             break
         elif installed.lower() == "no":
             print("You can either retry using the setup or manually install packages by running 'pip install -r requirements.txt. To quit the setup, enter 'quit' in the prompt. Once installed, enter yes in the prompt. ")
@@ -62,47 +64,45 @@ Retry the test.  """)
             print("Setup will now terminate. You can rerun the setup once all packages are installed...")
             quit()
         else:
-            print("Enter a valid option.")
+            print("Enter a valid option.\n")
 
     print("Building Docker image....")
     while True:
-        os.system(f"{engine} build . -t {imgName}")
-        imgInstall = os.popen(f'{engine} run {imgName} echo "Success"').read()
+        os.system(f"{ENGINE} build . -t {IMG_NAME}")
+        imgInstall = os.popen(f'{ENGINE} run {IMG_NAME} echo "Success"').read()
         if imgInstall == """Success\n""":
-            print("Image installation successful.")
+            print("Image installation successful.\n")
             break
         else:
             print(imgInstall)
-            print(f"It appears the image did not install properly. Make sure you have {engine.title()} installed and running. Linux users can try running 'sudo service {engine.lower()} start'. You can also build the image yourself by using '{engine.lower()} build . -t {imgName}' Be sure to keep name of image {imgName} else the app will not work. ")
+            print(f"It appears the image did not install properly. Make sure you have {ENGINE.title()} installed and running. Linux users can try running 'sudo service {ENGINE.lower()} start'. You can also build the image yourself by using '{ENGINE.lower()} build . -t {IMG_NAME}' Be sure to keep name of image {IMG_NAME} else the app will not work. ")
             retry = input("Retry? (Yes/No): ")
             if retry.lower() == "no":
                 break
-    print("All requirements have been successfully installed. Creating configuration file.")
-    configFile()           
-
-            
+    print("All requirements have been successfully installed.\n")
+    config_file()           
     
-def updateFull():
+def update_full():
     print("placeholdre")
   
-def updateImg():
+def update_img():
     print("Removing old image....")
-    os.system(f'{engine} rmi -f {imgName}')
+    os.system(f'{ENGINE} rmi -f {IMG_NAME}')
     print("Making new image...")
-    os.system(f'{engine} build . -t {imgName}')
-    if os.popen(f'{engine} run {imgName} echo "Success"').read() == "Success":
+    os.system(f'{ENGINE} build . -t {IMG_NAME}')
+    if os.popen(f'{ENGINE} run {IMG_NAME} echo "Success"').read() == "Success":
         print("Success")
     else:
         print("Fail")
     
-def updateFull():
+def update_full():
     print("placeholdre")
 
-def configFile():
+def config_file():
     print("Creating up config file....")
     f = open(".config", "w")
-    config = f'''PLATFORM: {platform.lower()}
-ENGINE: {engine.lower()}
+    config = f'''OS: {PLATFORM.lower()}
+ENGINE: {ENGINE.lower()}
 LAST_UPDATE: {datetime.now()}'''
     f.writelines(config)
     print("Configuration file generated.")
@@ -124,13 +124,13 @@ def main():
         option = int(input(("Enter a valid choice (1-5)")))
     
     elif option == '1':
-        setupReq()
+        setup_req()
     
     elif option == '2':
-        updateFull()
+        update_full()
     
     elif option == '3':
-        updateImg()
+        update_img()
     
     elif option == '4':
         disclaimer()
@@ -154,4 +154,4 @@ if __name__ == "__main__":
 # 6. Make config file
 # 7. Test run
 # 8. Add image update functionality
-# 9. 
+# 9. Add full update functionality 
