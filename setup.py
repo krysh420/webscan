a4a10bc5299309faf3f7e488738da571d35825c4
Krish <krishm848@gmail.com>
2024-07-20 18:20:53 +0530

    Every OS has its own problem

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ Retry the test.  """)
                     elif ENGINE.lower() == "podman": # Why should I install docker module if I use podman
                         if "docker" in i:
                             continue
-                    system(f"pip install {i} --ignore-requires-python --break-system-packages") # Linux users may have to use python3 instead of python (While I understand a lot of core packages of Linux still depend on Python2, it doesnt mean that Python2 should be default)
+                    system(f"pip install {i} --break-system-packages") # Linux users may have to use python3 instead of python (While I understand a lot of core packages of Linux still depend on Python2, it doesnt mean that Python2 should be default)
                 
                 f.close()
             
